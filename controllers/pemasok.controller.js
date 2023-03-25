82e404489910d866409843c4d8278c7c4d1abeab
Yanzen Project <yanwarsolah@gmail.com>
2023-03-26 01:22:39 +0700

    migrasi pemasok ke mysql

--- controllers/pemasok.controller.js
+++ controllers/pemasok.controller.js
@@ -1,118 +1,73 @@
 const express = require("express");
 const router = express.Router();
 const authentication = require("../middlewares/auth.middleware");
-const PemasokModel = require("../models/pemasok.model");
-const filterHelper = require("../helpers/filter.helper");
+const { knex } = require("../config/dbsql");
+
+const pemasokExist = async ({ kodePemasok }) => {
+  const pemasok = await knex("pemasok").where("kodePemasok", kodePemasok);
+  if (pemasok.length === 0) {
+    return false;
+  }
+
+  return pemasok;
+};
 
 router.post("/", [authentication], async (req, res) => {
   try {
-    const { kodePemasok, namaPemasok, alamatPemasok, teleponPemasok } =
-      req.body;
-
-    // Validate empty
-    if (!(kodePemasok && namaPemasok && alamatPemasok && teleponPemasok)) {
-      return res.status(400).json({
-        message: "Data harus lengkap",
-      });
-    }
-
-    // Validate kode pemasok unik
-    const oldPemasok = await PemasokModel.findOne({ kodePemasok });
-
-    if (oldPemasok) {
-      return res.status(400).json({
-        message: `Kode pemasok ${kodePemasok} sudah terdaftar.`,
-      });
-    }
-
-    await PemasokModel({
-      kodePemasok,
-      namaPemasok,
-      alamatPemasok,
-      teleponPemasok,
-    }).save();
-
-    return res
-      .status(201)
-      .json({ kodePemasok, namaPemasok, alamatPemasok, teleponPemasok });
+    await knex("pemasok").insert(req.body);
+    return res.status(201).json(req.body);
   } catch (error) {
-    console.error(error);
+    return res.status(400).json({ message: error.sqlMessage });
   }
 });
 
 router.get("/", [authentication], async (req, res) => {
   try {
-    const daftarPemasok = await PemasokModel.find(
-      filterHelper.getCleanQueryParams(req)
-    ).select({
-      _id: 0,
-      __v: 0,
-    });
+    const daftarPemasok = await knex.select().from("pemasok");
     return res.status(200).json(daftarPemasok);
   } catch (error) {
-    console.log(error);
+    return res.status(400).json(error);
   }
 });
 
 router.get("/:kodePemasok", [authentication], async (req, res) => {
   try {
-    const pemasok = await PemasokModel.findOne({
-      kodePemasok: req.params.kodePemasok,
-    }).select({
-      _id: 0,
-      __v: 0,
-    });
-
+    const pemasok = await pemasokExist(req.params);
     if (!pemasok) {
-      return res
-        .status(404)
-        .json({ message: `Pemasok ${req.params.kodePemasok} tidak tersedia` });
+      return res.status(404).json({ message: "404 not found" });
     }
-    return res.status(200).json(pemasok);
+    return res.status(200).json(pemasok[0]);
   } catch (error) {
-    console.log(error);
+    return res.status(400).json(error);
   }
 });
 
 router.put("/:kodePemasok", [authentication], async (req, res) => {
   try {
-    const kodePemasok = req.params.kodePemasok;
-    const pemasok = await PemasokModel.findOne({ kodePemasok });
-
+    const pemasok = await pemasokExist(req.params);
     if (!pemasok) {
-      return res
-        .status(404)
-        .json({ message: `Barang ${req.params.kodePemasok} tidak tersedia` });
+      return res.status(404).json({ message: "404 not found" });
     }
 
-    const { namaPemasok, alamatPemasok, teleponPemasok } = req.body;
-
-    await PemasokModel.findOneAndUpdate(
-      { kodePemasok },
-      { namaPemasok, alamatPemasok, teleponPemasok }
-    );
-    return res.status(200).json({ namaPemasok, alamatPemasok, teleponPemasok });
+    await knex("pemasok")
+      .where("kodePemasok", req.params.kodePemasok)
+      .update({ ...req.body });
+    return res.status(200).json(req.body);
   } catch (error) {
-    console.log(error);
+    return res.status(400).json(error);
   }
 });
 
 router.delete("/:kodePemasok", [authentication], async (req, res) => {
   try {
-    const kodePemasok = req.params.kodePemasok;
-
-    const pemasok = await PemasokModel.findOne({ kodePemasok });
-
+    const pemasok = await pemasokExist(req.params);
     if (!pemasok) {
-      return res
-        .status(404)
-        .json({ message: `Pemaosk ${kodePemasok} tidak tersedia` });
+      return res.status(404).json({ message: "404 not found" });
     }
-
-    await PemasokModel.findOneAndRemove({ kodePemasok });
+    await knex("pemasok").where({ kodePemasok: req.params.kodePemasok }).del();
     return res.status(204).json();
   } catch (error) {
-    console.log(error);
+    return res.status(400).json(error);
   }
 });
 
