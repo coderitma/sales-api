67109db0f4262d116d52c7f28e535d7e80884a60
Yanzen Project <yanwarsolah@gmail.com>
2023-03-27 00:18:41 +0700

    migrais transaksi pembelian

--- controllers/pembelian.controller.js
+++ controllers/pembelian.controller.js
@@ -1,58 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const authentication = require("../middlewares/auth.middleware");
-const PembelianModel = require("../models/pembelian.model");
-const { knex } = require("../config/dbsql");
 const ModelPembelian = require("../models/pembelian.model");
 
 router.post("/", [authentication], async (req, res) => {
   try {
-    await knex.transaction(async (trx) => {
-      const { pemasok, item, ...pembelian } = req.body;
-
-      // menghitung harga total dari quantity
-      // menyiapkan data item
-      let total = 0;
-      let dataInsertItemBeli = [];
-      for (let i = 0; i < item.length; i++) {
-        total += item[i].hargaBeli * item[i].jumlahBeli;
-        let temp = {
-          faktur: pembelian.faktur,
-          kodeBarang: item[i].kodeBarang,
-          namaBarang: item[i].namaBarang,
-          hargaBeli: item[i].hargaBeli,
-          hargaJual: item[i].hargaJual,
-          jumlahBeli: item[i].jumlahBeli,
-          subtotal: item[i].hargaBeli * item[i].jumlahBeli,
-        };
-        dataInsertItemBeli.push(temp);
-      }
-
-      // menyimpan pembelian
-      await knex("pembelian").transacting(trx).insert({
-        faktur: pembelian.faktur,
-        tanggal: pembelian.tanggal,
-        total: total,
-        kodePemasok: pemasok.kodePemasok,
-      });
-
-      await knex("item_beli")
-        .transacting(trx)
-        .insert([...dataInsertItemBeli]);
-
-      // update stock
-      for (let i = 0; i < item.length; i++) {
-        let data = await knex("barang")
-          .where("kodeBarang", item[i].kodeBarang)
-          .select();
-        await knex("barang")
-          .transacting(trx)
-          .where("kodeBarang", item[i].kodeBarang)
-          .update("jumlahBarang", data[0].jumlahBarang - item[i].jumlahBeli);
-      }
-    });
-
-    return res.status(201).json(req.body);
+    let result = await ModelPembelian.create(req.body);
+    return res.status(201).json(result);
   } catch (error) {
     return res.status(400).json(error);
   }
@@ -60,30 +14,37 @@ router.post("/", [authentication], async (req, res) => {
 
 router.get("/", [authentication], async (req, res) => {
   try {
-    let daftarPembelian = await ModelPembelian.list();
-    return res.status(200).json(daftarPembelian);
+    let page = req.query.page;
+    let limit = req.query.limit;
+    let faktur = req.query.faktur;
+    let kodePemasok = req.query.kodePemasok;
+    let result = await ModelPembelian.list(page, limit, faktur, kodePemasok);
+
+    return res
+      .set({
+        pagination: JSON.stringify(result.pagination),
+      })
+      .status(200)
+      .json(result.results);
   } catch (error) {
     console.error(error);
+    return res.status(400).json({ message: "Bad Request" });
   }
 });
 
 router.get("/:faktur", [authentication], async (req, res) => {
   try {
-    const pembelian = await PembelianModel.findOne({
-      faktur: req.params.faktur,
-    }).select({
-      _id: 0,
-      __v: 0,
-    });
-
+    let faktur = req.params.faktur;
+    let pembelian = await ModelPembelian.pembelianExist(faktur);
     if (!pembelian) {
-      return res
-        .status(404)
-        .json({ message: `Faktur ${req.params.faktur} tidak tersedia` });
+      return res.status(404).json({ message: "404 Not Found" });
     }
-    return res.status(200).json(pembelian);
+
+    let result = await ModelPembelian.get(faktur);
+    return res.status(200).json(result);
   } catch (error) {
     console.error(error);
+    return res.status(400).json({ message: "Bad Request" });
   }
 });
 
--- models/pembelian.model.js
+++ models/pembelian.model.js
@@ -1,55 +1,112 @@
 const { knex } = require("../config/dbsql");
+const ModelPemasok = require("./pemasok.model");
 
-const list = async () => {
-  try {
-    let daftarPembelian = await knex("pembelian").select();
-    let payload = [];
-    for (let pembelian of daftarPembelian) {
-      let data = {
-        faktur: pembelian.faktur,
-        tanggal: pembelian.tanggal,
-        total: pembelian.total,
-        pemasok: {},
-        item: [],
-      };
+const TABLE = "pembelian";
+const ModelPembelian = {};
 
-      let pemasok = (
-        await knex("pemasok")
-          .where("kodePemasok", pembelian.kodePemasok)
-          .select()
-      )[0];
-
-      data.pemasok = {
-        kodePemasok: pemasok.kodePemasok,
-        namaPemasok: pemasok.namaPemasok,
-        alamatPemasok: pemasok.alamatPemasok,
-        teleponPemasok: pemasok.teleponPemasok,
-      };
-
-      let daftarItem = await knex("item_beli").where(
-        "faktur",
-        pembelian.faktur
-      );
-      for (let item of daftarItem) {
-        data.item.push({
-          kodeBarang: item.kodeBarang,
-          namaBarang: item.namaBarang,
-          hargaBeli: item.hargaBeli,
-          hargaJual: item.hargaJual,
-          jumlahBeli: item.jumlahBeli,
-        });
-      }
-
-      payload.push(data);
-    }
-
-    return payload;
-  } catch (error) {
-    throw error;
+ModelPembelian.pembelianExist = async (faktur) => {
+  let pembelian = await knex(TABLE).where("faktur", faktur);
+  if (pembelian) {
+    return pembelian[0];
   }
 };
 
-const ModelPembelian = {
-  list,
+ModelPembelian.create = async (payload) => {
+  await knex.transaction(async (trx) => {
+    const { pemasok, item, ...pembelian } = payload;
+
+    // menghitung harga total dari quantity
+    // menyiapkan data item
+    let total = 0;
+    let dataInsertItemBeli = [];
+    for (let i = 0; i < item.length; i++) {
+      total += item[i].hargaBeli * item[i].jumlahBeli;
+      let temp = {
+        faktur: pembelian.faktur,
+        kodeBarang: item[i].kodeBarang,
+        namaBarang: item[i].namaBarang,
+        hargaBeli: item[i].hargaBeli,
+        hargaJual: item[i].hargaJual,
+        jumlahBeli: item[i].jumlahBeli,
+        subtotal: item[i].hargaBeli * item[i].jumlahBeli,
+      };
+      dataInsertItemBeli.push(temp);
+    }
+
+    // menyimpan pembelian
+    await knex(TABLE).transacting(trx).insert({
+      faktur: pembelian.faktur,
+      tanggal: pembelian.tanggal,
+      total: total,
+      kodePemasok: pemasok.kodePemasok,
+    });
+
+    await knex("item_beli")
+      .transacting(trx)
+      .insert([...dataInsertItemBeli]);
+
+    // update stock
+    for (let i = 0; i < item.length; i++) {
+      let data = await knex("barang")
+        .where("kodeBarang", item[i].kodeBarang)
+        .select();
+      await knex("barang")
+        .transacting(trx)
+        .where("kodeBarang", item[i].kodeBarang)
+        .update("jumlahBarang", data[0].jumlahBarang - item[i].jumlahBeli);
+    }
+  });
+
+  return payload;
 };
+
+ModelPembelian.list = async (page, limit, faktur, kodePemasok) => {
+  limit = limit ? parseInt(limit) : 10;
+  page = page ? parseInt(page) : 1;
+  if (page < 1) page = 1;
+  let offset = (page - 1) * limit;
+
+  let qb = knex(TABLE);
+  let qbCount = knex(TABLE);
+
+  if (faktur) {
+    qb = qb.whereLike("faktur", `%${faktur}%`);
+    qbCount = qbCount.whereLike("faktur", `%${faktur}%`);
+  }
+
+  if (kodePemasok) {
+    qb = db.whereLike("kodePemasok", `%${kodePemasok}%`);
+    qbCount = qbCount.whereLike("kodePemasok", `%${kodePemasok}%`);
+  }
+
+  let totalData = await qbCount.count("* as count").first();
+  results = await qb.limit(limit).offset(offset);
+  totalPage = Math.ceil(totalData.count / limit);
+  let prev = page - 1 > 0 ? page - 1 : null;
+  let next = page + 1 > totalPage ? null : page + 1;
+
+  return {
+    pagination: {
+      page,
+      limit,
+      next,
+      prev,
+    },
+    results,
+  };
+};
+
+ModelPembelian.get = async (faktur) => {
+  let pembelian = (await knex("pembelian").where("faktur", faktur))[0];
+  let pemasok = await ModelPemasok.get(pembelian.kodePemasok);
+  let daftarItem = await knex("item_beli").where("faktur", pembelian.faktur);
+
+  let payload = {
+    ...pembelian,
+    pemasok,
+    item: daftarItem,
+  };
+  return payload;
+};
+
 module.exports = ModelPembelian;
