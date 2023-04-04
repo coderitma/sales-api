e49f5f08ba780cfe82050f8c7eb465360b03f2a8
Yanzen Project <yanwarsolah@gmail.com>
2023-04-05 03:34:09 +0700

    Add config in utils but not implemented

---
+++ utils/config.js
@@ -0,0 +1,14 @@
+// JWT TOKEN CONFIG
+module.TOKEN_TIME = "10h";
+module.TOKEN_KEY =
+  "z%C*F-JaNdRgUkXp2r5u8x/A?D(G+KbPeShVmYq3t6v9y$B&E)H@McQfTjWnZr4u";
+
+// MARIA DB CONFIG
+module.DB_MARIA_NAME = "db_penjualan";
+module.DB_MARIA_HOST = "localhost";
+module.DB_MARIA_PASSWORD = "";
+module.DB_MARIA_USERNAME = "root";
+module.DB_MARIA_PORT = 3306;
+
+// MONGODB CONFIG
+module.MONGO_URI = "mongodb://localhost:27017/db_express_api_pembelian";
